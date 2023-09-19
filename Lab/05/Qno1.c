14108f0c07f987e577809a8d0b40629b4e728d0b
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2023-09-19 08:53:06 +0500

    Create Qno1.c

---
+++ Lab/05/Qno1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int a;
+printf("Enter any Number :");
+scanf("%d",&a);
+
+if(a%3=0){
+printf("This number is multiple of 3");
+}
+else{
+printf("This number is not a multiple of 3");
+}
+return 0;
+}
