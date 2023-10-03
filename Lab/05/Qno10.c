48deadeea213d6b827dc8676038b7e6d9300b326
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2023-10-03 19:43:35 +0500

    Update Qno10.c

--- Lab/05/Qno10.c
+++ Lab/05/Qno10.c
@@ -1,1 +1,13 @@
-\
+#include <stdio.h>
+
+int main()
+{
+    int bits,n,a,b;
+    printf("please enter a 8 bit number : ");
+     scanf("%d",&bits);
+     	 a =bits>>4;// number will be right shifted
+    	b =bits<<4;// number will be left shifted
+    	n = a | b;// this will add lower to the higher with out changing the bits
+    printf("number after replacing its bits is : %d", n);
+    return 0;
+}//end main
