53a6aab2a4ce26b76add8b311ea252060e3c220c
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2023-10-03 18:51:41 +0500

    Update Qno09.c

--- Lab/05/Qno09.c
+++ Lab/05/Qno09.c
@@ -10,7 +10,15 @@ int main(){
 	scanf("%f",&c);
 	
 	discriminant=b*b-(4*a*c);
-	printf("dicriminant is : %f", discriminant);
+	printf("dicriminant is : %f ", discriminant);
 	
+	if (discriminant<0){
+		printf("\n Roots are Imaginary ");
+	}
+	else if (discriminant=0){
+		printf("\n Roots are Equal ");
+	}
+	if (discriminant>0){
+		printf("\n Roots are Real and  ");
+	}
 	return 0;
-}
