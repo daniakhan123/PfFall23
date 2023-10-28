7b1e67ce8262dc94d40122f5fc05ecbdf93c9522
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2023-10-28 12:05:21 +0500

    Update q6.c

--- Assignment2/q6.c
+++ Assignment2/q6.c
@@ -1,1 +1,34 @@
 
+//programmer Dania khan
+//Date 28-10-23
+//Description pairs that makes the sum 't'
+
+#include <stdio.h>
+int main(){
+   int N,i,t,j,sum=0;
+
+    printf("enter size of an array : ");
+    scanf("%d",&N);
+
+    printf("enter targeted integer : ");
+    scanf("%d",&t);
+
+    int a[N];
+
+    for(i=0;i<N;i++){
+        printf("enter elements of array : ");
+        scanf("%d",&a[i]);
+    }
+    printf("THE PAIRS THAT MAKE THE SUM = %d ARE : \n",t);
+
+        for(i=0;i<N-1;i++){  
+            for(j=i+1;j<N;j++){
+            sum = a[i] + a[j]; //adding elements of an array
+            if(sum == t){ //checking ig=f their sum = t
+                printf("(%d , %d)\n",a[i],a[j]); //printing the sum
+            }
+            }
+    }
+
+    return 0;
+} //end main
