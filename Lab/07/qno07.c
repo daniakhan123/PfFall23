86b637371904e4fbbc26900d35e7c3329138fa92
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2023-10-28 11:05:23 +0500

    Update qno07.c

--- Lab/07/qno07.c
+++ Lab/07/qno07.c
@@ -1,24 +1,59 @@
-#include<stdio.h>
+//Programmer : Dania Khan
+//Date: 28-10-23
+//Description : Multiplying matrix
+
+#include <stdio.h>
+#include <math.h>
 int main(){
-	int arr[2][2];
-	int i,j,k,l;
-	
-	for(i=0;i<2;i++){
-		for(j=0;j<2;j++){
-			printf("enter elements of %d row and %d column of 1st matrix :",i,j);
-			scanf("%d",&arr[i][j]);
-		}
-	}
-	
-	
-	for(k=0;k<2;k++){
-		for(l=0;l<2;l++){
-			printf("enter elements of %d row and %d columnof 2nd matrix:",k,l);
-			scanf("%d",&arr[k][l]);
-		}
-	}
-	
-	
-	
-	return 0;
+
+    int r,c,m,n,i,j,k,p,q;
+
+    printf("enter size of row of 1st matrix : ");
+    scanf("%d", &r);
+    printf("enter size of column of 1st matrix : ");
+    scanf("%d", &c);
+
+
+    printf("enter size of row of 2nd matrix : ");
+    scanf("%d", &m);
+    printf("enter size of column of 2nd matrix : ");
+    scanf("%d", &n);
+
+  if(c!=m){//making condition for multiplication
+    printf("matrix cannot be multiplied ");
+  }
+ int  arr[r][c];//taking input array elements for 1st matrix
+   for(i=0;i<r;i++){
+    for(j=0;j<c;j++){
+        printf("enter elements of [%d][%d] of 1st matrix:",i,j);
+        scanf("%d",&arr[i][j]);
+    }
+   }
+   printf("\n");
+int arr2[m][n];//taking input array elements for 2nd matrix
+      for(p=0;p<r;p++){
+    for(q=0;q<c;q++){
+        printf("enter element of [%d][%d] of 2nd matrix:",p,q);
+        scanf("%d",&arr2[p][q]);
+    }
+   }
+  int res[r][n];
+
+     for(i=0;i<r;i++){
+     for(j=0;j<n;j++){
+        res[i][j]=0;
+         for(k=0;k<c;k++){
+
+        res[i][j] +=arr[i][k]*arr2[k][j];//multiplying matrix and adding it 
+         }
+    }  
+   }
+   printf("\n");//printing resultatnd matrix
+        for(i=0;i<r;i++){
+    for(j=0;j<n;j++){
+        printf("%d ",res[i][j]);
+    }
+    printf("\n");
 }
+    return 0;
+}//end main
