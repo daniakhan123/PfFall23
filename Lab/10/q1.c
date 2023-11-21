ae49879d6e111799453ec879793231a157b301bb
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2023-11-21 09:56:44 +0500

    Update q1.c

--- Lab/10/q1.c
+++ Lab/10/q1.c
@@ -15,35 +15,57 @@ struct Student {
 };
 
 int main() {
-    int n;
+    int n, pyear,rn;
     printf("Enter the number of students who joined the event: ");
     scanf("%d", &n);
+
     struct Student studentdata[n];
     int i;
 
-    for ( i = 0; i < n; ++i) {
-        printf("Enter Data of Student %d:\n", i + 1);
+    for (i = 0; i < n; ++i) {
+        printf("\nEnter Data of Student %d:\n", i + 1);
+
+        while (getchar() != '\n');
+
         printf("Name: ");
-        scanf("%s", studentdata[i].Name);
+        fgets(studentdata[i].Name, sizeof(studentdata[i].Name), stdin);
+
         printf("Roll Number: ");
         scanf("%f", &studentdata[i].Rollnum);
+
         while (getchar() != '\n');
+
         printf("Department: ");
         fgets(studentdata[i].Depart, sizeof(studentdata[i].Depart), stdin);
+
         printf("Course: ");
         fgets(studentdata[i].Course, sizeof(studentdata[i].Course), stdin);
+
         printf("Year: ");
         scanf("%f", &studentdata[i].year);
-    
     }
-    printf("\nEntered Students data:\n");
-    for (int j = 0; j < n; ++j) {
-        printf("\nStudent %d:\n", j + 1);
-        printf("Name: %s\n", studentdata[j].Name);
-        printf("Roll Number: %2f\n", studentdata[j].Rollnum);
-        printf("Department: %s", studentdata[j].Depart);
-        printf("Course: %s", studentdata[j].Course);
-        printf("Year: %2f\n", studentdata[j].year);
+
+    // Get the specific year to print
+    printf("Enter the specific year: ");
+    scanf("%d", &pyear);
+
+    // Print names of students who joined in a particular year
+    printf("\nStudent names who joined in the year %d:\n", pyear);
+    for (i = 0; i < n; ++i) {
+        if ((int)studentdata[i].year == pyear) {
+            printf("Name: %s\n", studentdata[i].Name);
+        }
+    }
+    printf("specific year : ");
+    scanf("%d",&rn);
+          printf("\nStudent data of roll number %d:\n", rn);
+        for (i = 0; i < n; ++i) {
+        if ((int)studentdata[i].Rollnum == rn) {
+            printf("Name: %s\n", studentdata[i].Name);
+            printf("Department: %s\n", studentdata[i].Depart);
+            printf("Course: %s\n", studentdata[i].Course);
+            printf("Year of Joining: %2f\n", studentdata[i].year);
+        }
     }
 
     return 0;
