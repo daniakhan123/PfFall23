4b2ab6a3159434855e30bec476d958e6e53f7eb6
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2023-11-22 01:37:59 +0500

    Update q1.c

--- Lab/10/q1.c
+++ Lab/10/q1.c
@@ -1,3 +1,45 @@
 // programmer Dania khan
 // date 17/11/23
 // desc 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Declare the function prototype for malloc
+void* my_malloc(size_t size);
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of values: ");
+    scanf("%d", &n);
+
+    int *arr = (int*)my_malloc(n * sizeof(int));
+
+    for (i = 0; i < n; i++) {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Entered values are: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Free the allocated memory
+    free(arr);
+
+    return 0;
+}
+
+// Define the my_malloc function
+void* my_malloc(size_t size) {
+    int *ptr = (int*)malloc(size);
+    if (ptr == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(EXIT_FAILURE);
+    }
+    return ptr;
+}
