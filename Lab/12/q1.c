fcc36eb8ad114b74af41ddac81137804f8ff6cda
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2023-12-09 13:41:27 +0500

    Update q1.c

--- Lab/12/q1.c
+++ Lab/12/q1.c
@@ -1,35 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//programmer : dania khan
+// desc: reading csv file and and printing number of rows and columns in it also giving the tabular output.
 
-int main(){
-FILE *fptr;
-fptr = fopen("./test1.c", "r+");
 
-if (fptr == NULL){
-printf ("The file is not opened.The program will exit now");
-return 1;
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int c;
+    int countc = 0;
+    int i,j;
+    int countr = 1;
+    FILE *fp = NULL;
+
+    fp = fopen("tab.csv", "r");
+
+    if (fp == NULL) {
+        printf("File not found.\n");
+        exit(1);
+    }
+
+    while ((c = fgetc(fp)) != EOF) {
+        if (c == '\n') {
+            countr++;
+        }
+    }
+
+    fseek(fp, 0, SEEK_SET);
+
+    // Read the first line (header) and discard it
+    while ((c = fgetc(fp)) != '\n' && c != EOF) {
+        // Discard the header line
+    }
+
+    // Read the second line (data) to count columns
+    while ((c = fgetc(fp)) != '\n' && c != EOF) {
+        if (c == ',') {
+            countc++;
+        }
+    }
+
+    // Add 1 to count the last column
+    countc++;
+
+    printf("Number of rows: %d\n", countr);
+    printf("Number of columns: %d\n", countc);
+
+        fseek(fp, 0, SEEK_SET);
+
+        for(i=0;i<countr;i++){
+            for(j=0;j<countc;j++){
+                fscanf(fp,"%d, ",&c);
+                printf("| %d\t",c);
+            }
+            printf("\n");
+        }
+
+
+
+    fclose(fp);
+
+    return 0;
 }
 
-else{
-int rows=1, columns=0;
-int i;
-char chr = fgetc(fptr);
-while (chr!=EOF){
-if (chr == '\n'){
-rows = rows + 1;
-columns = 0;
-}
-chr = fgetc(fptr);
-columns++;
-}
-
-fclose (fptr);
-printf ("Number of rows is: %d\n",rows);
-printf ("Number of columns is: %d",columns);
-return 0;
-}
-
-
-return 0;
-}
